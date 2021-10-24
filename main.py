e297250da6a91d956e111ede9d604a5a1f1a1172
barasnevicius <barasnevicius@gmail.com>
2021-10-23 21:25:57 -0300

    Criação da Base

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from pygame.locals import *
 
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 800
-
+SPEED = 10
+GRAVITY = 1
+GAME_SPEED = 10
 class Bird(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -11,6 +13,8 @@ class Bird(pygame.sprite.Sprite):
         self.images = [pygame.image.load('assets/yellowbird-upflap.png').convert_alpha(),
                        pygame.image.load('assets/yellowbird-midflap.png').convert_alpha(),
                        pygame.image.load('assets/yellowbird-downflap.png').convert_alpha()]
+
+        self.speed = SPEED
 
         self.current_image = 0
 
@@ -24,25 +28,64 @@ class Bird(pygame.sprite.Sprite):
         self.current_image = (self.current_image + 1) % 3
         self.image = self.images[self.current_image]
 
+        self.speed += GRAVITY
+
+        # UPDATE HEIGHT
+        self.rect[1] += self.speed
+
+    def bump(self):
+        self.speed = -SPEED
+
+class Ground(pygame.sprite.Sprite):
+    def __init__(self, width, height, xpos):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load('assets/base.png')
+        self.image = pygame.transform.scale(self.image, (width , height))
+
+        self.rect = self.image.get_rect()
+        self.rect[0] = xpos
+        self.rect[1] = SCREEN_HEIGHT - height
+
+    def update(self):
+        self.rect[0] -= GAME_SPEED
+
 pygame.init()
 screen = pygame.display.set_mode ((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 BACKGROUND = pygame.image.load('assets/background-night.png')
 BACKGROUND = pygame.transform.scale(BACKGROUND,(SCREEN_WIDTH, SCREEN_HEIGHT))
 
+# BIRD
 bird_group = pygame.sprite.Group()
 bird = Bird()
 bird_group.add(bird)
 
+# FLOOR
+ground_group = pygame.sprite.Group()
+
+for i in range(2):
+    ground = Ground(2 * SCREEN_WIDTH, 100, 2 * SCREEN_WIDTH * i)
+    ground_group.add(ground)
+
+clock = pygame.time.Clock()
+
+# GAME LOOP
 while True:
+    clock.tick(20)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
+        if event.type == KEYDOWN:
+            if event.key == K_SPACE:
+                bird.bump()
 
     screen.blit(BACKGROUND,(0,0))
 
     bird_group.update()
+    ground_group.update()
 
     bird_group.draw(screen)
+    ground_group.draw(screen)
 
     pygame.display.update()
